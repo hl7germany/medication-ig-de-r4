7bb81d75cd739243af26f07a0f2cee22429bb086
florianschoffke <florian.schoffke@gematik.de>
2026-07-30 08:22:55 +0200

    set version to 2.0.0

--- scripts/medication-dosage-to-text.py
+++ scripts/medication-dosage-to-text.py
@@ -40,7 +40,7 @@ import os
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-__version__ = "2.0.1"
+__version__ = "2.0.0"
 __language__ = "de-DE"
 
 class MedicationDosageTextGenerator:
